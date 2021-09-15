78db7591c1bb8f16864e4646eae0b535db99d5ec
pratiktalele1 <pratiktalele1@gmail.com>
2021-09-15 10:45:53 +0530

    Welcome to Employee Payroll App

---
+++ src/main/java/com/bridgelabz/eps/SpringEmployeePayrollAppApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.eps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmployeePayrollAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEmployeePayrollAppApplication.class, args);
+		System.out.println("Hello user from SpringEmployeePayrollAppApplication..!");
+	}
+
+}
